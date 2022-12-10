6df436d3c569594abecbfc9be89ab908e9dbf44c
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-11 05:18:46 +0900

    メール送信

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'sasaki2031@gmail.com'
+  layout 'mailer'
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :contacts
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
